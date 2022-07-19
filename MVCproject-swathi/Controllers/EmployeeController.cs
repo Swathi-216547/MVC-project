4af2637728efa08728963e650dcb30a1c22c7458
Swati <swatireddy0229@gmail.com>
2022-07-19 17:07:22 +0530

    commiting mvc project
    comiting all files from MVC

---
+++ MVCproject-swathi/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCproject_swathi.Models;
+
+
+namespace MVCproject_swathi.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        // GET:Employee
+        public ActionResult Employeeinfo()
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeId = 216547,
+                EmployeeName = "Swathi",
+                EmployeeLocation = "Bangalore"
+            };
+                
+            
+            return View (employee);
+        }
+    }
+}
---
+++ MVCproject-swathi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCproject_swathi.MvcApplication" Language="C#" %>
